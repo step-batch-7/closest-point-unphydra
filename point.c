7e5fbb078405198528828b936c083f4621280ad5
rivu <58026024+unphydra@users.noreply.github.com>
2020-04-13 14:49:05 +0530

    modified pont struct assignment in point.c

--- point.c
+++ point.c
@@ -10,16 +10,14 @@ double get_distance(Point a, Point b)
 void get_closest_food(Point food_points[], int points_length, Point current_location, Point *closest_food_location)
 {
   double distance = get_distance(current_location,food_points[0]);
-  closest_food_location->x = food_points[0].x;
-  closest_food_location->y = food_points[0].y;
+  *closest_food_location = food_points[0];
   for (int i = 1; i < points_length; i++)
   {
     double current_distance = get_distance(current_location,food_points[i]);
     if(current_distance < distance)
     {
       distance = current_distance;
-      closest_food_location->x = food_points[i].x;
-      closest_food_location->y = food_points[i].y;
+      *closest_food_location = food_points[i];
     }
   }
 }
